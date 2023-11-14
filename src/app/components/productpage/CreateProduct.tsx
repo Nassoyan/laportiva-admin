dc6df440ad6e363ace3a21dadff74427884f5f5f
Tigran Nasoyan <tigrannasoyan@Tigrans-MacBook-Air.local>
2023-11-15 01:51:20 +0400

    brand_id logic is complete

--- src/app/components/productpage/CreateProduct.tsx
+++ src/app/components/productpage/CreateProduct.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { Brands } from "../../pages/brands/BrandsPage"
 
 const styles = {
     display: "flex",
@@ -13,28 +14,48 @@ const buttonStyle = {
 }
 
 const CreateProduct: React.FC = () => {
+  const [brandData, setBrandData] = useState<Brands[]>()
+  const [defaultValue, setDefaultValue] = useState()
+
     const [inputText, setInputText] = useState({
         name: "",
         price: "",
         artikul: "",
         code: "",
-        brand_id: "",
+        brand_id: "Select Brand",
         product_images: "", // initialize to null
       });
       const [btn, setBtn] = useState(true);
       const navigate = useNavigate()
     
       useEffect(() => {
+        
+
         const { name, price, artikul, code, brand_id, product_images } = inputText;
         if (name && price && artikul && code && brand_id && product_images) {
           setBtn(false);
         } else {
           setBtn(true);
         }
+        console.log("1useEffect");
+        
       }, [inputText]);
+
+      useEffect(() => {
+        fetch("http://localhost:3000/brands")
+                      .then(res => res.json())
+                      .then((res: Brands[]) => setBrandData(res))
+                      .catch(err => console.error(err));
+      }, [])
+
+      
+
+     
+
     
       function handleChange(e: React.ChangeEvent<HTMLInputElement> | any) {
         const { value, name } = e.target;
+        console.log(value, "vallll")
     
         if (e.target?.files) {
           setInputText((prev: any) => ({
@@ -48,6 +69,7 @@ const CreateProduct: React.FC = () => {
           }));
         }
       }
+
     
       async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -60,18 +82,15 @@ const CreateProduct: React.FC = () => {
         formData.append("brand_id", inputText.brand_id);
         formData.append("product_images", inputText.product_images);
         
-    
         try {
           await fetch("http://localhost:3000/products", {
             method: "POST",
-            body: formData, // send the formData instead of JSON
-          }).then((res) => res.ok && navigate("/products"));
-          
+            body: formData, 
+          })
+          // .then((res) => res.ok && navigate("/products"));
         } catch (err) {
           console.error(err);
         }
-    
-        // Add any necessary error handling or success messages here
       }
     
 
@@ -88,32 +107,39 @@ const CreateProduct: React.FC = () => {
                 <form onSubmit={handleSubmit} style={{display:"flex", flexDirection:"column", gap:"15px"}} method="post" encType="multipart/form-data">
 
                 <div className="form-group">
-                  <label>Product Name</label>
-                  <input name="name" value={inputText.name} onChange={handleChange} type="text" className="form-control"  placeholder="Product Name"/>
+                  <label>Product Name*</label>
+                  <input required name="name" value={inputText.name} onChange={handleChange} type="text" className="form-control"  placeholder="Product Name"/>
                 </div>
 
                 <div className="form-group">
-                  <label>Product Price</label>
-                  <input name="price" value={inputText.price} onChange={handleChange} type="text" className="form-control"  placeholder="Product Price"/>
+                  <label>Product Price*</label>
+                  <input required name="price" value={inputText.price} onChange={handleChange} type="number" min="0" className="form-control"  placeholder="Product Price"/>
                 </div>
 
                 <div className="form-group">
-                  <label>Product Artikul</label>
-                  <input name="artikul" value={inputText.artikul} onChange={handleChange} type="text" className="form-control"  placeholder="Product Artikul"/>
+                  <label>Product Artikul*</label>
+                  <input required name="artikul" value={inputText.artikul} onChange={handleChange} type="text" className="form-control"  placeholder="Product Artikul"/>
                 </div>
 
                 <div className="form-group">
-                  <label>Product Code</label>
-                  <input name="code" value={inputText.code} onChange={handleChange} type="text" className="form-control"  placeholder="Product Code"/>
+                  <label>Product Code*</label>
+                  <input required name="code" value={inputText.code} onChange={handleChange} type="text" className="form-control"  placeholder="Product Code"/>
                 </div>
 
                 <div className="form-group">
-                  <label>Brand ID</label>
-                  <input name="brand_id" value={inputText.brand_id} onChange={handleChange} type="text" className="form-control"  placeholder="Brand ID"/>
+                  <label>Brand ID*</label>
+                  <select name="brand_id" className="form-control" value={inputText.brand_id} onChange={handleChange}>
+                    <option value="Select Brand" disabled>Select Brand</option>
+                    {brandData?.map((brand) => {
+                      return (
+                        <option  value={brand.id} key={brand.id}>{brand.name}</option>
+                      );
+                    })}
+                  </select>
                 </div>
                 
                 <div className="form-group">
-                  <label>Product Image</label>
+                  <label>Product Image*</label>
                   <input name="product_images"  onChange={handleChange} type="file" className="form-control" />
                 </div>
 
--- src/app/components/productpage/Pagination.tsx
+++ src/app/components/productpage/Pagination.tsx
@@ -1,31 +1,40 @@
 import React from 'react'
 
-function Pagination({pageNumbers, currentPage, setCurrentPage}) {
-    console.log(pageNumbers);
-    const pages = Array.from({ length: pageNumbers }, (_, index) => index + 1);
 
+function Pagination({ totalPages,  currentPage, setCurrentPage }) {
+    const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
     
-  return (
-    <nav aria-label="Page navigation example">
-        <ul className="pagination">
-            <li onClick={() => {
-                if(currentPage <= 1) return false
-                setCurrentPage(currentPage - 1)
-            }} className="page-item"><a className="page-link" href="#">Previous</a></li>
-            {pages.map((page, i) => {
-                return (
-                    <li key={i} onClick={(e) => {
-                        setCurrentPage(i + 1)
-                    }} className={`page-item ${currentPage === page ? "active" : ""}`}><a className="page-link" href="#">{page}</a></li>
-                )
-            })}
-            <li onClick={() => {
-                if(currentPage >= pages.length) return false;
-                setCurrentPage(currentPage + 1)
-                }}  className="page-item"><a className="page-link" href="#">Next</a></li>
-        </ul>
-    </nav>
-  )
+    return (
+        <nav aria-label="Page navigation example">
+            <ul className="pagination">
+                <li onClick={() => {
+                    if(currentPage <= 1) return false
+                    setCurrentPage(currentPage - 1)
+                    }} className="page-item">
+                    <button className="page-link">Previous</button>
+                </li>
+                {pages.map((page, i) => {
+                    return (
+                        <li
+                            key={i}
+                            onClick={() => {
+                                setCurrentPage(page)
+                            }} // Update the currentPage state on click
+                            className={`page-item ${currentPage === page ? 'active' : ''}`}
+                        >
+                            <p className="page-link">{page}</p>
+                        </li>
+                    );
+                })}
+                <li onClick={() => {
+                    if(currentPage >= totalPages) return false
+                    setCurrentPage(currentPage + 1)
+                    }} className="page-item">
+                    <button className="page-link">Next</button>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
-export default Pagination
+export default Pagination;
